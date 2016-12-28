d1baa8b78c62e1bf5b5a15f21a60ec9a184006e7
Ly <lynguyenthanhhoaioi@gmail.com>
2016-12-28 14:16:16 +0700

    Update INS_TKBieu.sql

--- Stored Procedure/INS_TKBieu.sql
+++ Stored Procedure/INS_TKBieu.sql
@@ -11,7 +11,7 @@ BEGIN
 	SELECT @Ph = MAX(MaMP) FROM MuonPhong 
 	while (@lap > @i)
 	BEGIN
-		INSERT INTO TKBieu (MaPhong, [MaLH], [TietBD], [TietKT], [Ngay], [MaMP]) VALUES (@maphong, 1, @bd, @kt, @date, @Ph)
+		INSERT INTO TKBieu (MaPhong, [MaLH], [TietBD], [TietKT], [Ngay], [MaMP], [MaTP]) VALUES (@maphong, 1, @bd, @kt, @date, @Ph, 1)
 		set @date = dateadd(day, 7, @date)
 		SET @i = @i + 1
 	END
